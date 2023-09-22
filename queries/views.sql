968900ea2ee6a83cd443784d630a03f378cfe0a3
Augusto <augustotsantos@outlook.com>
2023-09-22 14:18:20 -0300

    att view de quantidade de notas

--- queries/views.sql
+++ queries/views.sql
@@ -5,6 +5,16 @@ FROM rst_restaurante;
 
 -- View v_quantidade_notas_restaurante
 CREATE VIEW v_quantidade_notas_restaurante AS
-SELECT rst_nota, COUNT(*) AS quantidade
-FROM rst_restaurante
-GROUP BY rst_nota;
+SELECT
+    notas.nota AS nota,
+    COUNT(rst.rst_nota) AS quantidade
+FROM (
+    SELECT 1 AS nota FROM dual UNION ALL
+    SELECT 2 AS nota FROM dual UNION ALL
+    SELECT 3 AS nota FROM dual UNION ALL
+    SELECT 4 AS nota FROM dual UNION ALL
+    SELECT 5 AS nota FROM dual
+) notas
+LEFT JOIN rst_restaurante rst ON notas.nota = rst.rst_nota
+GROUP BY notas.nota
+ORDER BY notas.nota;
