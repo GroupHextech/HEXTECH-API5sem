ce84e7393e29de232f94a795b3cee3398d7a366f
Augusto <augustotsantos@outlook.com>
2023-10-10 22:49:49 -0300

    att no bd para views futuras

--- queries/bd.sql
+++ queries/bd.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2023-10-10 20:11:58.434
+-- Last modification date: 2023-10-11 01:36:40.034
 
 -- tables
 -- Table: cat_categoria
@@ -12,7 +12,7 @@ CREATE TABLE cat_categoria (
 -- Table: con_confeccao
 CREATE TABLE con_confeccao (
     con_id integer  NOT NULL,
-    con_qtd_utilizada float(4)  NOT NULL,
+    con_qtd_utilizada float(4)  NULL,
     est_id integer  NOT NULL,
     pra_id integer  NOT NULL,
     CONSTRAINT con_confeccao_pk PRIMARY KEY (con_id)
@@ -23,6 +23,7 @@ CREATE TABLE est_estoque (
     est_id integer  NOT NULL,
     est_nome varchar2(30)  NULL,
     est_qtd_estoque float(4)  NULL,
+    est_nivel_minimo float(4)  NULL,
     uni_id integer  NOT NULL,
     cat_categoria_cat_id integer  NOT NULL,
     CONSTRAINT ite_item_pk PRIMARY KEY (est_id)
@@ -31,10 +32,10 @@ CREATE TABLE est_estoque (
 -- Table: for_fornecedor
 CREATE TABLE for_fornecedor (
     for_id integer  NOT NULL,
-    for_nome varchar2(50)  NOT NULL,
-    for_telefone varchar2(15)  NOT NULL,
-    for_endereco varchar2(100)  NOT NULL,
-    for_email varchar2(100)  NOT NULL,
+    for_nome varchar2(50)  NULL,
+    for_telefone varchar2(15)  NULL,
+    for_endereco varchar2(100)  NULL,
+    for_email varchar2(100)  NULL,
     CONSTRAINT for_fornecedor_pk PRIMARY KEY (for_id)
 ) ;
 
@@ -44,7 +45,7 @@ CREATE TABLE fun_funcionario (
     fun_nome varchar2(50)  NULL,
     fun_funcao varchar2(50)  NULL,
     fun_salario float(2)  NULL,
-    fun_folga date  NOT NULL,
+    fun_folga date  NULL,
     fun_data_contrato date  NULL,
     CONSTRAINT fun_funcionario_pk PRIMARY KEY (fun_id)
 ) ;
@@ -52,13 +53,21 @@ CREATE TABLE fun_funcionario (
 -- Table: ins_insumo
 CREATE TABLE ins_insumo (
     ins_id integer  NOT NULL,
-    ins_qtd_entrada float(4)  NOT NULL,
-    ins_validade date  NOT NULL,
-    ins_data_hora_compra timestamp  NOT NULL,
-    ins_valor_compra float(2)  NOT NULL,
+    ins_qtd_entrada float(4)  NULL,
+    ins_validade date  NULL,
+    ins_data_hora_compra timestamp  NULL,
+    ins_valor_compra float(2)  NULL,
     for_id integer  NOT NULL,
     est_id integer  NOT NULL,
     CONSTRAINT ins_insumo_pk PRIMARY KEY (ins_id)
+) ;
+
+-- Table: ite_item_pedido
+CREATE TABLE ite_item_pedido (
+    pra_id integer  NOT NULL,
+    ped_id integer  NOT NULL,
+    ite_quantidade integer  NULL,
+    CONSTRAINT ite_item_pedido_pk PRIMARY KEY (pra_id,ped_id)
 ) ;
 
 -- Table: ped_pedido
@@ -67,7 +76,7 @@ CREATE TABLE ped_pedido (
     ped_hora_pedido timestamp  NULL,
     ped_hora_entregue timestamp  NULL,
     ped_valor_total float(2)  NULL,
-    pra_id integer  NOT NULL,
+    ped_avaliacao integer  NULL,
     fun_id integer  NOT NULL,
     res_id integer  NOT NULL,
     CONSTRAINT ped_pedido_pk PRIMARY KEY (ped_id)
@@ -76,8 +85,8 @@ CREATE TABLE ped_pedido (
 -- Table: pra_prato
 CREATE TABLE pra_prato (
     pra_id integer  NOT NULL,
-    pra_custo float(2)  NOT NULL,
-    pra_preco_venda float(2)  NOT NULL,
+    pra_custo float(2)  NULL,
+    pra_preco_venda float(2)  NULL,
     pra_tempo_preparo timestamp  NULL,
     CONSTRAINT pra_prato_pk PRIMARY KEY (pra_id)
 ) ;
@@ -85,11 +94,11 @@ CREATE TABLE pra_prato (
 -- Table: res_reserva
 CREATE TABLE res_reserva (
     res_id integer  NOT NULL,
-    res_nome varchar2(50)  NOT NULL,
-    res_telefone varchar2(15)  NOT NULL,
+    res_nome varchar2(50)  NULL,
+    res_telefone varchar2(15)  NULL,
     res_mesa integer  NULL,
     res_qtd_pesssoas integer  NULL,
-    res_data_hora timestamp  NOT NULL,
+    res_data_hora timestamp  NULL,
     sta_id integer  NOT NULL,
     CONSTRAINT res_reserva_pk PRIMARY KEY (res_id)
 ) ;
@@ -97,19 +106,29 @@ CREATE TABLE res_reserva (
 -- Table: sta_status
 CREATE TABLE sta_status (
     sta_id integer  NOT NULL,
-    sta_nome varchar2(30)  NULL,
+    sta_nome varchar2(30)  NOT NULL,
     CONSTRAINT sta_status_pk PRIMARY KEY (sta_id)
 ) ;
 
 -- Table: uni_unidade
 CREATE TABLE uni_unidade (
     uni_id integer  NOT NULL,
-    uni_nome varchar2(30)  NULL,
+    uni_nome varchar2(30)  NOT NULL,
     uni_sigla char(2)  NULL,
     CONSTRAINT uni_unidade_pk PRIMARY KEY (uni_id)
 ) ;
 
 -- foreign keys
+-- Reference: Table_13_ped_pedido (table: ite_item_pedido)
+ALTER TABLE ite_item_pedido ADD CONSTRAINT Table_13_ped_pedido
+    FOREIGN KEY (ped_id)
+    REFERENCES ped_pedido (ped_id);
+
+-- Reference: Table_13_pra_prato (table: ite_item_pedido)
+ALTER TABLE ite_item_pedido ADD CONSTRAINT Table_13_pra_prato
+    FOREIGN KEY (pra_id)
+    REFERENCES pra_prato (pra_id);
+
 -- Reference: con_confeccao_est_estoque (table: con_confeccao)
 ALTER TABLE con_confeccao ADD CONSTRAINT con_confeccao_est_estoque
     FOREIGN KEY (est_id)
@@ -144,11 +163,6 @@ ALTER TABLE ins_insumo ADD CONSTRAINT ins_insumo_ite_item
 ALTER TABLE ped_pedido ADD CONSTRAINT ped_pedido_fun_funcionario
     FOREIGN KEY (fun_id)
     REFERENCES fun_funcionario (fun_id);
-
--- Reference: ped_pedido_pra_prato (table: ped_pedido)
-ALTER TABLE ped_pedido ADD CONSTRAINT ped_pedido_pra_prato
-    FOREIGN KEY (pra_id)
-    REFERENCES pra_prato (pra_id);
 
 -- Reference: ped_pedido_res_reserva (table: ped_pedido)
 ALTER TABLE ped_pedido ADD CONSTRAINT ped_pedido_res_reserva
--- queries/inserts.sql
+++ queries/inserts.sql
@@ -1,97 +1,47 @@
--- Inserir dados na tabela for_fornecedor utilizando auto_increment
-INSERT INTO for_fornecedor (for_nome) VALUES ('Fornecedor A');
-INSERT INTO for_fornecedor (for_nome) VALUES ('Fornecedor B');
-INSERT INTO for_fornecedor (for_nome) VALUES ('Fornecedor C');
-INSERT INTO for_fornecedor (for_nome) VALUES ('Fornecedor D');
-INSERT INTO for_fornecedor (for_nome) VALUES ('Fornecedor E');
+-- Inserir dados na tabela cat_categoria
+INSERT INTO cat_categoria (cat_nome) VALUES ('Categoria 1');
+INSERT INTO cat_categoria (cat_nome) VALUES ('Categoria 2');
 
--- Inserir dados na tabela ite_item utilizando auto_increment
-INSERT INTO ite_item (ite_nome) VALUES ('Item 1');
-INSERT INTO ite_item (ite_nome) VALUES ('Item 2');
-INSERT INTO ite_item (ite_nome) VALUES ('Item 3');
-INSERT INTO ite_item (ite_nome) VALUES ('Item 4');
-INSERT INTO ite_item (ite_nome) VALUES ('Item 5');
-INSERT INTO ite_item (ite_nome) VALUES ('Item 6');
-INSERT INTO ite_item (ite_nome) VALUES ('Item 7');
+-- Inserir dados na tabela con_confeccao
+INSERT INTO con_confeccao (con_qtd_utilizada, est_id, pra_id) VALUES (10.5, 1, 1);
+INSERT INTO con_confeccao (con_qtd_utilizada, est_id, pra_id) VALUES (15.2, 2, 2);
 
--- Inserir dados na tabela ins_insumo utilizando auto_increment
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (100, '2023-12-31', NOW(), 10.50, 1, 1);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (200, '2023-05-31', NOW(), 15.75, 2, 2);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (150, '2023-11-30', NOW(), 12.25, 3, 3);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (250, '2023-07-15', NOW(), 18.50, 4, 4);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (180, '2023-12-10', NOW(), 14.75, 5, 5);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (200, '2023-12-10', NOW(), 14.75, 6, 5);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (50, '2023-12-10', NOW(), 14.75, 1, 3);
-INSERT INTO ins_insumo (ins_quantidade, ins_validade, ins_data_hora_compra, ins_valor_compra, ite_id, for_id)
-VALUES (180, '2023-12-10', NOW(), 14.75, 3, 2);
+-- Inserir dados na tabela est_estoque
+INSERT INTO est_estoque (est_nome, est_qtd_estoque, est_nivel_minimo, uni_id, cat_categoria_cat_id) VALUES ('Item 1', 100.0, 10.0, 1, 1);
+INSERT INTO est_estoque (est_nome, est_qtd_estoque, est_nivel_minimo, uni_id, cat_categoria_cat_id) VALUES ('Item 2', 200.0, 20.0, 2, 2);
 
--- Inserir dados na tabela res_reserva utilizando auto_increment
-INSERT INTO res_reserva (res_nome, res_mesa, res_telefone, res_data_hora, res_status)
-VALUES ('João', 3, '(27) 92859-4796', '2023-09-26 19:00:00', 'reservada');
+-- Inserir dados na tabela for_fornecedor
+INSERT INTO for_fornecedor (for_nome, for_telefone, for_endereco, for_email) VALUES ('Fornecedor 1', '123-456-7890', 'Endereço 1', 'fornecedor1@example.com');
+INSERT INTO for_fornecedor (for_nome, for_telefone, for_endereco, for_email) VALUES ('Fornecedor 2', '987-654-3210', 'Endereço 2', 'fornecedor2@example.com');
 
-INSERT INTO res_reserva (res_nome, res_mesa, res_telefone, res_data_hora, res_status)
-VALUES ('Maria', 5, '(54) 93244-5588', '2023-09-26 19:00:00', 'reservada');
+-- Inserir dados na tabela fun_funcionario
+INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_folga, fun_data_contrato) VALUES ('Funcionário 1', 'Cargo 1', 3000.00, TO_DATE('2023-10-11', 'YYYY-MM-DD'), TO_DATE('2023-01-15', 'YYYY-MM-DD'));
+INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_folga, fun_data_contrato) VALUES ('Funcionário 2', 'Cargo 2', 2500.00, TO_DATE('2023-10-12', 'YYYY-MM-DD'), TO_DATE('2023-02-20', 'YYYY-MM-DD'));
 
-INSERT INTO res_reserva (res_nome, res_mesa, res_telefone, res_data_hora, res_status)
-VALUES ('Pedro', 2, '(79) 93585-7273', '2023-09-26 20:00:00', 'reservada');
+-- Inserir dados na tabela ins_insumo
+INSERT INTO ins_insumo (ins_qtd_entrada, ins_validade, ins_data_hora_compra, ins_valor_compra, for_id, est_id) VALUES (50.0, TO_DATE('2024-01-01', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-10-11 08:00:00', 'YYYY-MM-DD HH24:MI:SS'), 100.00, 1, 1);
+INSERT INTO ins_insumo (ins_qtd_entrada, ins_validade, ins_data_hora_compra, ins_valor_compra, for_id, est_id) VALUES (75.0, TO_DATE('2024-02-01', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-10-11 10:30:00', 'YYYY-MM-DD HH24:MI:SS'), 150.00, 2, 2);
 
-INSERT INTO res_reserva (res_nome, res_mesa, res_telefone, res_data_hora, res_status)
-VALUES ('Ana', 4, '(24) 93413-4223', '2023-09-27 17:00:00', 'reservada');
+-- Inserir dados na tabela ite_item_pedido
+INSERT INTO ite_item_pedido (pra_id, ped_id, ite_quantidade) VALUES (1, 1, 2);
+INSERT INTO ite_item_pedido (pra_id, ped_id, ite_quantidade) VALUES (2, 2, 3);
 
-INSERT INTO res_reserva (res_nome, res_mesa, res_telefone, res_data_hora, res_status)
-VALUES ('Carlos', 6, '(51) 93600-6275', '2023-09-27 17:00:00', 'reservada');
+-- Inserir dados na tabela ped_pedido
+INSERT INTO ped_pedido (ped_hora_pedido, ped_hora_entregue, ped_valor_total, ped_avaliacao, fun_id, res_id) VALUES (TO_TIMESTAMP('2023-10-11 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP('2023-10-11 13:30:00', 'YYYY-MM-DD HH24:MI:SS'), 50.00, 5, 1, 1);
+INSERT INTO ped_pedido (ped_hora_pedido, ped_hora_entregue, ped_valor_total, ped_avaliacao, fun_id, res_id) VALUES (TO_TIMESTAMP('2023-10-11 19:00:00', 'YYYY-MM-DD HH24:MI:SS'), NULL, 75.00, NULL, 2, 2);
 
--- Inserir dados na tabela pra_prato utilizando auto_increment
-INSERT INTO pra_prato (pra_nome, pra_custo, pra_preco_venda, res_id)
-VALUES ('Prato A', 7.50, 15.00, 1);
+-- Inserir dados na tabela pra_prato
+INSERT INTO pra_prato (pra_custo, pra_preco_venda, pra_tempo_preparo) VALUES (10.00, 20.00, TO_TIMESTAMP('00:30:00', 'HH24:MI:SS'));
+INSERT INTO pra_prato (pra_custo, pra_preco_venda, pra_tempo_preparo) VALUES (15.00, 30.00, TO_TIMESTAMP('00:45:00', 'HH24:MI:SS'));
 
-INSERT INTO pra_prato (pra_nome, pra_custo, pra_preco_venda, res_id)
-VALUES ('Prato B', 8.00, 16.50, 2);
+-- Inserir dados na tabela res_reserva
+INSERT INTO res_reserva (res_nome, res_telefone, res_mesa, res_qtd_pesssoas, res_data_hora, sta_id) VALUES ('Reserva 1', '111-222-3333', 5, 4, TO_TIMESTAMP('2023-10-12 19:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1);
+INSERT INTO res_reserva (res_nome, res_telefone, res_mesa, res_qtd_pesssoas, res_data_hora, sta_id) VALUES ('Reserva 2', '222-333-4444', 7, 6, TO_TIMESTAMP('2023-10-13 20:00:00', 'YYYY-MM-DD HH24:MI:SS'), 2);
 
-INSERT INTO pra_prato (pra_nome, pra_custo, pra_preco_venda, res_id)
-VALUES ('Prato C', 9.50, 18.00, 3);
+-- Inserir dados na tabela sta_status
+INSERT INTO sta_status (sta_nome) VALUES ('Status 1');
+INSERT INTO sta_status (sta_nome) VALUES ('Status 2');
 
-INSERT INTO pra_prato (pra_nome, pra_custo, pra_preco_venda, res_id)
-VALUES ('Prato D', 8.75, 17.50, 4);
-
-INSERT INTO pra_prato (pra_nome, pra_custo, pra_preco_venda, res_id)
-VALUES ('Prato E', 10.00, 20.00, 5);
-
--- Inserir dados na tabela rst_restaurante utilizando auto_increment
-INSERT INTO rst_restaurante (rst_nome, rst_nota, pra_id, ins_id)
-VALUES ('Restaurante X', 4, 1, 1);
-
-INSERT INTO rst_restaurante (rst_nome, rst_nota, pra_id, ins_id)
-VALUES ('Restaurante X', 5, 2, 2);
-
-INSERT INTO rst_restaurante (rst_nome, rst_nota, pra_id, ins_id)
-VALUES ('Restaurante X', 4, 3, 3);
-
-INSERT INTO rst_restaurante (rst_nome, rst_nota, pra_id, ins_id)
-VALUES ('Restaurante X', 5, 4, 4);
-
-INSERT INTO rst_restaurante (rst_nome, rst_nota, pra_id, ins_id)
-VALUES ('Restaurante X', 3, 5, 5);
-
--- Inserir dados na tabela fun_funcionario utilizando auto_increment
-INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_desempenho, fun_folga)
-VALUES ('Funcionário 1', 'Garçom', 2000.00, 9, '2023-09-30 18:00:00');
-
-INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_desempenho, fun_folga)
-VALUES ('Funcionário 2', 'Cozinheiro', 2500.00, 8, '2023-09-29 20:00:00');
-
-INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_desempenho, fun_folga)
-VALUES ('Funcionário 3', 'Garçom', 2200.00, 8, '2023-09-28 17:30:00');
-
-INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_desempenho, fun_folga)
-VALUES ('Funcionário 4', 'Cozinheiro', 2700.00, 9, '2023-09-27 19:00:00');
-
-INSERT INTO fun_funcionario (fun_nome, fun_funcao, fun_salario, fun_desempenho, fun_folga)
-VALUES ('Funcionário 5', 'Atendente', 2000.00, 7, '2023-09-26 18:30:00');
+-- Inserir dados na tabela uni_unidade
+INSERT INTO uni_unidade (uni_nome, uni_sigla) VALUES ('Unidade 1', 'U1');
+INSERT INTO uni_unidade (uni_nome, uni_sigla) VALUES ('Unidade 2', 'U2');
